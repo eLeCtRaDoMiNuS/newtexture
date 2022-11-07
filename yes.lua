42663b276bf914a921f25fed3735b454cdda8f12
llameiq <77410796+eLeCtRaDoMiNuS@users.noreply.github.com>
2022-11-07 21:06:02 +0300

    Create yes.lua

---
+++ yes.lua
@@ -0,0 +1,59 @@
+game:GetObjects("rbxassetid://7062163349")[1].Parent = workspace
+
+workspace.Sword.Name = "Handle"
+workspace.Handle.Size = Vector3.new(0.03, 1.886, 1.385)
+local attach = game.ReplicatedStorage.Items["wood_sword"]["Handle"]["RightGripAttachment"]:Clone()
+
+game:GetService("ReplicatedStorage").Items["wood_sword"].Handle:Destroy()
+
+workspace.Handle.Parent = game:GetService("ReplicatedStorage").Items["wood_sword"]
+attach.Parent = game:GetService("ReplicatedStorage").Items["wood_sword"].Handle
+attach.Position = Vector3.new(-4.37114e-08, 2.08716, 0.996195)
+attach.Axis = Vector3.new(-0.104528, -8.74228e-08, -0.994522)
+attach.Rotation = Vector3.new(180, -90, 0)
+attach.CFrame = CFrame.new(-4.37113989e-08, 2.08716011, 0.996195018, -4.37113883e-08, 0, -1, 8.74227766e-08, -1, -3.82137093e-15, -1, -8.74227766e-08, 4.37113883e-08)
+-- stone
+game:GetObjects("rbxassetid://7062163349")[1].Parent = workspace
+
+workspace.Sword.Name = "Handle"
+workspace.Handle.Size = Vector3.new(0.03, 1.886, 1.385)
+local attach = game.ReplicatedStorage.Items["stone_sword"]["Handle"]["RightGripAttachment"]:Clone()
+
+game:GetService("ReplicatedStorage").Items["stone_sword"].Handle:Destroy()
+
+workspace.Handle.Parent = game:GetService("ReplicatedStorage").Items["stone_sword"]
+attach.Parent = game:GetService("ReplicatedStorage").Items["stone_sword"].Handle
+attach.Position = Vector3.new(-4.37114e-08, 2.08716, 0.996195)
+attach.Axis = Vector3.new(-0.104528, -8.74228e-08, -0.994522)
+attach.Rotation = Vector3.new(180, -90, 0)
+attach.CFrame = CFrame.new(-4.37113989e-08, 2.08716011, 0.996195018, -4.37113883e-08, 0, -1, 8.74227766e-08, -1, -3.82137093e-15, -1, -8.74227766e-08, 4.37113883e-08)
+-- iron
+game:GetObjects("rbxassetid://7062163349")[1].Parent = workspace
+
+workspace.Sword.Name = "Handle"
+workspace.Handle.Size = Vector3.new(0.03, 1.886, 1.385)
+local attach = game.ReplicatedStorage.Items["iron_sword"]["Handle"]["RightGripAttachment"]:Clone()
+
+game:GetService("ReplicatedStorage").Items["iron_sword"].Handle:Destroy()
+
+workspace.Handle.Parent = game:GetService("ReplicatedStorage").Items["iron_sword"]
+attach.Parent = game:GetService("ReplicatedStorage").Items["iron_sword"].Handle
+attach.Position = Vector3.new(-4.37114e-08, 2.08716, 0.996195)
+attach.Axis = Vector3.new(-0.104528, -8.74228e-08, -0.994522)
+attach.Rotation = Vector3.new(180, -90, 0)
+attach.CFrame = CFrame.new(-4.37113989e-08, 2.08716011, 0.996195018, -4.37113883e-08, 0, -1, 8.74227766e-08, -1, -3.82137093e-15, -1, -8.74227766e-08, 4.37113883e-08)
+-- diamond
+game:GetObjects("rbxassetid://7062163349")[1].Parent = workspace
+
+workspace.Sword.Name = "Handle"
+workspace.Handle.Size = Vector3.new(0.03, 1.886, 1.385)
+local attach = game.ReplicatedStorage.Items["diamond_sword"]["Handle"]["RightGripAttachment"]:Clone()
+
+game:GetService("ReplicatedStorage").Items["diamond_sword"].Handle:Destroy()
+
+workspace.Handle.Parent = game:GetService("ReplicatedStorage").Items["diamond_sword"]
+attach.Parent = game:GetService("ReplicatedStorage").Items["diamond_sword"].Handle
+attach.Position = Vector3.new(-4.37114e-08, 2.08716, 0.996195)
+attach.Axis = Vector3.new(-0.104528, -8.74228e-08, -0.994522)
+attach.Rotation = Vector3.new(180, -90, 0)
+attach.CFrame = CFrame.new(-4.37113989e-08, 2.08716011, 0.996195018, -4.37113883e-08, 0, -1, 8.74227766e-08, -1, -3.82137093e-15, -1, -8.74227766e-08, 4.37113883e-08)
